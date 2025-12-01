c343d11c031d0d85de5ac8602e43bb0c8933913c
Lorenzo Gravina <lorenzo.gravina@me.com>
2025-12-01 21:35:48 +0100

    Quartz sync: Dec 1, 2025, 9:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.lorenzogravina.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
